c433320475bfb252b14ab2a2e55fa438345a6fbc
yuiseki <yuiseki@users.noreply.github.com>
2022-05-29 09:30:48 +0000

    deploy: e2b2f44eb8c7254e0eec37cfb6da4dd420c62e33

---
+++ _next/static/l3QEe8nVzlietUqzQNE1K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-677cf7bd01cbc75d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/[title]":["static/chunks/pages/[title]-46da0d04177f8a83.js"],sortedPages:["/","/_app","/_error","/[title]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
