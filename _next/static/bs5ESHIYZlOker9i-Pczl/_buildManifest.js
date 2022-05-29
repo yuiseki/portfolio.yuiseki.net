3db6ab3472f69903bf99f9b87a78eb55dffa1315
yuiseki <yuiseki@users.noreply.github.com>
2022-05-29 09:25:53 +0000

    deploy: 1cc5c1f71572f112ac421c890c58acc0b2683f05

---
+++ _next/static/bs5ESHIYZlOker9i-Pczl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e103a32f15fc481c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/[title]":["static/chunks/pages/[title]-b4e88bd6d253c119.js"],sortedPages:["/","/_app","/_error","/[title]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/fNtPp18cYjaU8kBGMizFZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f3bd037faf79685a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/[title]":["static/chunks/pages/[title]-f9d060733e2fa3eb.js"],sortedPages:["/","/_app","/_error","/[title]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
